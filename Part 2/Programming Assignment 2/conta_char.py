48a14e31eaa85a55ab2014db9148dff91c7547ac
Andre Pinto <61568351+andre6293@users.noreply.github.com>
2020-07-08 00:54:06 -0300

    Update conta_char.py

--- Part 2/Programming Assignment 2/conta_char.py
+++ Part 2/Programming Assignment 2/conta_char.py
@@ -1,5 +1,12 @@
-def conta_letras(frase, contar='vogais'):
-    pass
+def conta_letras(frase, contar="vogais"):
+    vogais = ["a", "á", "ã", "à", "e", "é", "ê", "i", "í", "o", "ô", "õ", "ó", "u", "ú"]
+    letras = len(frase) - frase.count(" ")
+    total_vogais = 0
 
-
-print(conta_letras("Claudio"))
+    for i in range(len(frase)):
+        if frase[i].lower() in vogais:
+               total_vogais += 1
+    if contar == "vogais":
+        return total_vogais
+    else:
+        return letras - total_vogais
